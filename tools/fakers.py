43bcbcbc83bb14cbf30c4051978b03c3b5d9abaa
alexander-romanikhin <romanikhin.a@yandex.ru>
2026-09-19 17:38:29 +0300

    Добавлены генераторы ФИО

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,4 +1,23 @@
 import time
+import random
 
 def get_random_email() -> str:
     return f'test.{time.time()}@example.com'
+
+# Списки имен и фамилий
+names = ["Алексей", "Мария", "Иван", "Елена", "Дмитрий", "Ольга"]
+surnames = ["Смирнов", "Иванова", "Петров", "Кузнецова", "Соколов", "Попова"]
+middlenames = ["Александрович", "Алексеевич", "Анатольевич", "Борисович",
+    "Валерьевич", "Васильевич"]
+
+def get_random_username() -> str:
+    random_name = random.choice(names)
+    return random_name
+
+def get_random_surname() -> str:
+    random_surname = random.choice(surnames)
+    return random_surname
+
+def get_random_middle_name() -> str:
+    random_middle_name = random.choice(middlenames)
+    return random_middle_name
